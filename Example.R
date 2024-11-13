67d96e82da7a6645afd274d997c197f9845b69bf
parolidiego <parolidiego@gmail.com>
2024-11-13 16:29:39 +0100

    Arranged df and export
    As I commented on an issue, I have arranged the df and I have exported it.

--- Example.R
+++ Example.R
@@ -1,4 +1,7 @@
 library(dplyr)
-starwars_new <- starwars |> filter(mass > 60) |> select(name, mass, height)
+starwars_new <- starwars |> 
+  filter(mass > 60) |> 
+  select(name, mass, height) |> 
+  arrange(mass)
 library(readr)
-write_csv(starwars_new, file = "./data/starwars_new.csv")
+write_csv(starwars_new, file = "./starwars_new.csv")
